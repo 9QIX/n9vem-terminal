1c2e24121a6a6cbfba11d50ef4e9c698aaa73255
Novem <70324910+9QIX@users.noreply.github.com>
2023-11-08 16:52:51 +0800

    Update user_profile.ps1

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -21,6 +21,7 @@ Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionSource ListView
 
 # Fzf
 Import-Module PSFzf
